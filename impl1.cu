a556f5d4774366ccd48e2d80fba1b6d8a2eaa3de
Corentin Rejaud <crejaud@nbp-137-247.nbp.ruw.rutgers.edu>
2017-03-27 14:06:32 -0400

    added tests

--- impl1.cu
+++ impl1.cu
@@ -13,11 +13,28 @@ __global__ void pulling_kernel(std::vector<initial_vertex> * peeps, int offset, 
 }
 
 void puller(std::vector<initial_vertex> * peeps, int blockSize, int blockNum){
+    /* Allocate here... */
+    unsigned int *cuda_edges_src, *cuda_edges_dest, *cuda_edges_weight;
+    unsigned int edges_length;
+    unsigned int vertices_length = peeps.size();
+    unsigned int *cuda_distance_prev = (unsigned int *) malloc(peeps.size() * sizeof(unsigned int));
+    unsigned int *cuda_distance_cur = (unsigned int *) malloc(peeps.size() * sizeof(unsigned int));
+
+    int i = 0;
+    for(auto const& vertex: peeps) {
+      for(auto const& edge: vertex.nbrs) {
+        std::cout << "vertexIndex: " << i << " | vertexDistance: " << vertex.get_vertex_ref().distance << " | src: " << edge.srcIndex << " | dest: " << " | weight: " << edge.edgeValue.weight << endl;
+      }
+      i++;
+    }
+
     setTime();
 
     /*
      * Do all the things here!
      **/
+
+
 
     std::cout << "Took " << getTime() << "ms.\n";
 }
