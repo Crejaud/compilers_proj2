b92fbfc9c7ba4f2a65e0de8b0c1172f4949114b6
Corentin Rejaud <crejaud@nbp-183-121.nbp.ruw.rutgers.edu>
2017-03-29 19:26:32 -0400

    dsa

--- impl2.cu
+++ impl2.cu
@@ -345,23 +345,25 @@ void neighborHandler(std::vector<initial_vertex> * peeps, int blockSize, int blo
 
       printf("past forloop\n");
 
-      cudaMemcpy(noChange, cuda_noChange, sizeof(int), cudaMemcpyDeviceToHost);
-      if (*noChange == TRUE) break;
-      *noChange = TRUE;
-      cudaMemcpy(cuda_noChange, noChange, sizeof(int), cudaMemcpyHostToDevice);
+      if (i != 1) {
+        cudaMemcpy(noChange, cuda_noChange, sizeof(int), cudaMemcpyDeviceToHost);
+        if (*noChange == TRUE) break;
+        *noChange = TRUE;
+        cudaMemcpy(cuda_noChange, noChange, sizeof(int), cudaMemcpyHostToDevice);
 
-      printf("past nochange reset\n");
+        printf("past nochange reset\n");
 
-      // get current distance and copy it to both cuda_distance_prev and cuda_distance_cur
-      cudaMemcpy(distance_cur, cuda_distance_cur, vertices_length * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-      cudaMemcpy(cuda_distance_prev, distance_cur, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
-      cudaMemcpy(cuda_distance_cur, distance_cur, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
+        // get current distance and copy it to both cuda_distance_prev and cuda_distance_cur
+        cudaMemcpy(distance_cur, cuda_distance_cur, vertices_length * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(cuda_distance_prev, distance_cur, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
+        cudaMemcpy(cuda_distance_cur, distance_cur, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
 
-      printf("past distance reset\n");
+        printf("past distance reset\n");
 
-      cudaMemcpy(is_distance_infinity, cuda_is_distance_infinity_cur, vertices_length * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-      cudaMemcpy(cuda_is_distance_infinity_prev, is_distance_infinity, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
-      cudaMemcpy(cuda_is_distance_infinity_cur, is_distance_infinity, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
+        cudaMemcpy(is_distance_infinity, cuda_is_distance_infinity_cur, vertices_length * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(cuda_is_distance_infinity_prev, is_distance_infinity, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
+        cudaMemcpy(cuda_is_distance_infinity_cur, is_distance_infinity, vertices_length * sizeof(unsigned int), cudaMemcpyHostToDevice);
+      }
 
       printf("starting outcore\n");
 
